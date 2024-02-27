8603fb8b4e20b7c1172e033c448ca2ed296a2819
djsnipa1 djsnipa1 <24595995-djsnipa1djsnipa@users.noreply.replit.com>
2024-02-27 00:04:33 +0000

    repo in correct state now

--- replit.nix
+++ replit.nix
@@ -1,22 +1,22 @@
 { pkgs }: {
     deps = [
+      pkgs.neovim
       pkgs.pastel
-     # pkgs.pastebinit
-    #  pkgs.neovim
+    # pkgs.pastebinit
       pkgs.zip
       pkgs.nodePackages.pnpm
       pkgs.nodejs-slim_21
       pkgs.lf
       pkgs.lazygit
       pkgs.eza
-     # pkgs.httplz
+    # pkgs.httplz
       pkgs.gh
       pkgs.nodePackages.svelte-language-server
       pkgs.nodePackages.vscode-html-languageserver-bin
       pkgs.nodePackages.vscode-css-languageserver-bin
-      pkgs.yt-dlp
-      pkgs.ffmpeg_5-full
-      pkgs.mediainfo
-      pkgs.exiftool
+    # pkgs.yt-dlp
+    # pkgs.ffmpeg_5-full
+    # pkgs.mediainfo
+    # pkgs.exiftool
     ];
 }
