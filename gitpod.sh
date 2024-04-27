aa5cd028827138a54fcdfb4e70c070a92e7d7e12
Chad Boyce <chadboyce@gmail.com>
2024-04-27 00:01:40 +0000

    added tmux menus to gitpod.sh

--- gitpod.sh
+++ gitpod.sh
@@ -16,3 +16,15 @@ echo "installed lazyvim"
 
 gp ports visibility 5173:public
 gp ports list --no-color
+
+# add tmux plugin
+# The file you want to modify
+file="$HOME/.tmux_plugins.conf"
+# The line you want to add
+line="set -g @plugin 'jaclu/tmux-menus'"
+# The line number you want to add the line at
+line_number=8
+# Use sed to insert the line
+sed -i "${line_number}i${line}" "$file"
+# source tmux config
+tmux source-file $HOME/.tmux.conf
