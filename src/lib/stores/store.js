75070faf8ac5459fabf00d4ccef0445d0792863d
Chad Boyce <chadboyce@gmail.com>
2024-05-02 10:14:40 +0000

    added functionality to the nudge buttons

--- src/lib/stores/store.js
+++ src/lib/stores/store.js
@@ -5,7 +5,26 @@ export const youtubeUrl = writable('https://youtu.be/9B1SQX9a_hU');
 // export const videoId = writable('M7lc1UVf-VE');
 export const videoId = writable('9B1SQX9a_hU');
 
-export const cssPosition = writable('');
+export const cssPosition = writable(0);
+// nudges cssPosition left in FineTuning.svelte
+export function nudgeLeft() {
+ cssPosition.update(value => {
+    if (value - 3 < -33) {
+      return -33;
+    } else {
+      return value - 3;
+    }
+ });
+}
+export function nudgeRight() {
+ cssPosition.update(value => {
+    if (value + 3 > 33) {
+      return 33;
+    } else {
+      return value + 3;
+    } 
+ });
+}
 
 export const isControlsOpen = writable(false);
 
