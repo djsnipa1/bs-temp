2b3c1ce5d4f3383f854e3bf745624fb051f807e8
Chad Boyce <chadboyce@gmail.com>
2024-05-04 23:12:14 +0000

    fixed youtube url video id getting

--- src/lib/components/InputBoxFinal.svelte
+++ src/lib/components/InputBoxFinal.svelte
@@ -1,26 +1,17 @@
 <script>
   import { PasteButton } from '$lib';
   import { isUrlOpen, videoId, menuOpen } from '$lib/stores/store.js';
+  import getVideoId from 'get-video-id';
 
   $: currentBind = '';
   let newId;
 
   function getYouTubeID(url) {
-    // Define a regular expression to match different YouTube URL formats
-    var regex =
-      /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    // Use the match() method to extract the video ID
-    var match = String(url).match(regex);
-    // Check if the match is valid and the ID is 11 characters long
-    if (match && match[7].length == 11) {
-      // Return the video ID
-      return match[7];
-    } else {
-      // Return an empty string or an error message
-      return '';
-    }
-  }
 
+    const { id } = getVideoId(url); //=> 'dQw4w9WgXcQ'
+    
+    return id;
+}
   // Test the function with some sample URLs
   console.log(getYouTubeID('https://www.youtube.com/watch?v=qeMFqkcPYcg')); // qeMFqkcPYcg
   console.log(getYouTubeID('https://youtu.be/qeMFqkcPYcg')); // qeMFqkcPYcg
@@ -48,14 +39,15 @@
 </script>
 
 <div class="container glass flex h-16 items-center justify-center">
-  <PasteButton
+<!--  <PasteButton
     class="button mx-4 flex-1 rounded-md bg-slate-300 p-1 text-slate-700 shadow-md hover:bg-slate-400 hover:text-slate-800"
   />
+  -->
 
   <input
     type="text"
     placeholder="https://youtu.be/m_xoN8KlP3w"
     on:input={handleInput}
-    class="input input-sm input-bordered mr-4 w-full max-w-xs"
+    class="input input-sm input-bordered w-full max-w-xs"
   />
 </div>
