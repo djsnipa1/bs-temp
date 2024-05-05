38faffce04254631be57b8b288f05c3636bcbf5a
Chad Boyce <chadboyce@gmail.com>
2024-05-05 11:59:44 +0000

    working on intro

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -1,26 +1,39 @@
 <script>
-  import anime from 'animejs';
-  import { onMount } from 'svelte';
+  import anime from "animejs";
+  import {onMount} from "svelte";
 
-  let logo;
+  let logo, circleMask;
 
   function start() {
     anime
       .timeline({
-        easing: 'easeOutExpo',
+        easing: "easeOutExpo",
         autoplay: true,
-        loop: true
+        loop: true,
       })
       .add({
         targets: logo,
-        scale: [0, 1],
-        duration: 1000
-      });
+        opacity: [ {value: 1, duration: 1} ], 
+        scale: [ {value: [0, 1], duration: 1000} ]
+        
+      })
+      .add({
+        targets: circleMask,
+        scale: [{value: [18, 0], duration: 1500}]
+      })
+      .add({
+        targets: logo,
+        opacity: 0,
+        scale: .6,
+        duration: 200,
+        easing: "linear"
+      }, 1550);
   }
 
   onMount(start);
 </script>
 
-<div class="flex h-screen w-full items-center justify-center">
-  <h1 class="font-kiona text-2xl" bind:this={logo}>Suckage</h1>
+<div class="w-full h-screen flex items-center justify-center">
+  <div class="absolute rounded-full h-20 w-20 border-[20px] border-blue-400" bind:this={circleMask}></div>
+  <h1 class="text-2xl font-kiona opacity-0" bind:this="{logo}">Suckage</h1>
 </div>
